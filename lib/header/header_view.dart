c4f2b06fa47c53aa545a3061245d194585e89e0c
Freeda Moore <freedamoore@outlook.com>
2020-07-17 15:31:26 +1000

    added responsive_builder and auto_size_text packages and made header more responsive

--- lib/header/header_view.dart
+++ lib/header/header_view.dart
@@ -1,5 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class HeaderView extends StatelessWidget {
   const HeaderView({
@@ -10,64 +12,115 @@ class HeaderView extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-    return Container(
-      height: height,
-      width: width,
-      color: Colors.black,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Flutter Web Experiment',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 60,
-                        textStyle: TextStyle(color: Colors.white),
-                      )),
-                  Text('by Freeda Moore',
-                      style: GoogleFonts.montserrat(
-                        fontSize: 60,
-                        textStyle: TextStyle(color: Colors.redAccent[100]),
-                      )),
-                  SizedBox(height: 40),
-                  Text(
-                    'My first attempt at building a Web app using Flutter and Dart.',
-                    style: TextStyle(fontSize: 24, color: Colors.white),
-                  ),
-                  SizedBox(height: 40),
-                  FlatButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(7),
-                        ),
-                      ),
-                      color: Colors.redAccent[100],
-                      onPressed: () {},
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 17,
-                          horizontal: 15,
-                        ),
-                        child: Text(
-                          'Continue...',
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.white,
-                          ),
-                        ),
-                      )),
-                ],
+
+    return ResponsiveBuilder(
+      builder: (_, size) {
+        if (size.isMobile) return HeaderMobileView();
+        return Container(
+          height: height,
+          width: width,
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 100.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: HeaderBody(),
+                ),
+                FlutterLogo(
+                  size: width * 0.4,
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class HeaderBody extends StatelessWidget {
+  final isMobile;
+  const HeaderBody({
+    Key key,
+    this.isMobile = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AutoSizeText(
+          'Flutter Web Experiment',
+          style: GoogleFonts.montserrat(
+            fontSize: 60,
+            textStyle: TextStyle(color: Colors.white),
+          ),
+          maxLines: 2,
+        ),
+        AutoSizeText(
+          'by Freeda Moore',
+          style: GoogleFonts.montserrat(
+            fontSize: 60,
+            textStyle: TextStyle(color: Colors.redAccent[100]),
+          ),
+          maxLines: 1,
+        ),
+        SizedBox(height: isMobile ?? false ? 20 : 40),
+        AutoSizeText(
+          'My first attempt at building a responsive web app using Flutter and Dart.',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+          maxLines: 3,
+        ),
+        SizedBox(height: isMobile ?? false ? 20 : 40),
+        FlatButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(7),
               ),
             ),
-            FlutterLogo(
-              size: 300,
-            )
-          ],
-        ),
+            color: Colors.redAccent[100],
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: isMobile ?? false ? 10 : 17,
+                horizontal: isMobile ?? false ? 8 : 15,
+              ),
+              child: Text(
+                'Continue...',
+                style: TextStyle(
+                  fontSize: isMobile ?? false ? 20 : 24,
+                  color: Colors.white,
+                ),
+              ),
+            )),
+      ],
+    );
+  }
+}
+
+class HeaderMobileView extends StatelessWidget {
+  const HeaderMobileView({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Container(
+      height: height * 0.9,
+      width: width,
+      color: Colors.black,
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+      child: Column(
+        children: [
+          FlutterLogo(size: height * 0.3),
+          Spacer(),
+          HeaderBody(isMobile: true),
+        ],
       ),
     );
   }
